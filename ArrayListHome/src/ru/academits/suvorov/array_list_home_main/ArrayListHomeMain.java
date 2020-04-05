558d3e1c1f1688335da5ac41015343af1f6127e0
suvor <cedjhjd150792>
2020-04-06 01:28:02 +0700

    Задача ArrayListHome

--- ArrayListHome/src/ru/academits/suvorov/array_list_home_main/ArrayListHomeMain.java
+++ ArrayListHome/src/ru/academits/suvorov/array_list_home_main/ArrayListHomeMain.java
@@ -1,21 +1,21 @@
 package ru.academits.suvorov.array_list_home_main;
 
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayListHomeMain {
-    public static ArrayList<String> getStringsList(String fileAddress) {
+    public static ArrayList<String> getFileStrings(String filePath) {
         ArrayList<String> stringsList = new ArrayList<>();
 
-        try (Scanner scanner = new Scanner(new FileInputStream(fileAddress))) {
+        try (Scanner scanner = new Scanner(new FileInputStream(filePath))) {
             while (scanner.hasNextLine()) {
                 stringsList.add(scanner.nextLine());
             }
-        } catch (IOException e) {
-            System.out.println("Не удается найти указанный файл: " + fileAddress);
+        } catch (FileNotFoundException e) {
+            System.out.println("Не удается найти указанный файл: " + filePath);
             System.out.println();
         }
 
@@ -30,7 +30,7 @@ public class ArrayListHomeMain {
         }
     }
 
-    public static ArrayList<Integer> getListWithoutRepetition(ArrayList<Integer> integerList) {
+    public static ArrayList<Integer> getListWithoutRepetitions(ArrayList<Integer> integerList) {
         ArrayList<Integer> integerListWithoutRepetitions = new ArrayList<>();
 
         for (Integer e : integerList) {
@@ -43,14 +43,14 @@ public class ArrayListHomeMain {
     }
 
     public static void main(String[] args) {
-        ArrayList<String> stringsList = getStringsList("./ArrayListHome/strings.txt");
+        ArrayList<String> stringsList = getFileStrings("./ArrayListHome/strings.txt");
 
         System.out.println("Чтение из файла и запись в Array List: " + stringsList);
         System.out.println();
 
         ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(1, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 9));
 
-        System.out.println("Удаление из списка повторяющихся чисел: " + getListWithoutRepetition(numbersList));
+        System.out.println("Удаление из списка повторяющихся чисел: " + getListWithoutRepetitions(numbersList));
         System.out.println();
 
         removeEvenNumbers(numbersList);
