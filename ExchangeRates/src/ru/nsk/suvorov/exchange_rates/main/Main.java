98fdf7c57f943420cfa39d020b5bbb06fca2be42
79994 <suvorov150792@gmail.com>
2020-06-09 00:16:14 +0700

    test

--- ExchangeRates/src/ru/nsk/suvorov/exchange_rates/main/Main.java
+++ ExchangeRates/src/ru/nsk/suvorov/exchange_rates/main/Main.java
@@ -4,10 +4,13 @@ import ru.nsk.suvorov.exchange_rates.model.File;
 import ru.nsk.suvorov.exchange_rates.view.Frame;
 import ru.nsk.suvorov.exchange_rates.view.View;
 
+import java.net.MalformedURLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         File file = new File();
 
+        file.readURlFile("http://assets.cavexp.com/test.txt");
         file.readFile("ExchangeRates/src/input.txt");
         file.readFile("ExchangeRates/src/test1.txt");
 
--- ExchangeRates/src/ru/nsk/suvorov/exchange_rates/model/File.java
+++ ExchangeRates/src/ru/nsk/suvorov/exchange_rates/model/File.java
@@ -1,30 +1,44 @@
 package ru.nsk.suvorov.exchange_rates.model;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class File {
     ArrayList<String[]> list = new ArrayList<>();
 
-    public void readFile(String nameFile) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(nameFile))) {
-            String line = reader.readLine();
-
-            while (line != null) {
-                if (line.isEmpty()) {
-                    line = reader.readLine();
-
-                    continue;
-                }
-
-                list.add(line.split(";"));
-
-                line = reader.readLine();
-            }
+    public void readURlFile(String nameFile) throws MalformedURLException {
+        URL url = new URL(nameFile);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()))) {
+            addLineArray(reader);
         } catch (IOException e) {
             System.out.println("Не удается найти указанный файл: " + nameFile);
+        }
+    }
+
+    public void readFile(String nameFile) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(nameFile))) {
+            addLineArray(reader);
+        } catch (IOException e) {
+            System.out.println("Не удается найти указанный файл: " + nameFile);
+        }
+    }
+
+
+    private void addLineArray (BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+
+        while (line != null) {
+            if (line.isEmpty()) {
+                line = reader.readLine();
+
+                continue;
+            }
+
+            list.add(line.split(";"));
+
+            line = reader.readLine();
         }
     }
 
